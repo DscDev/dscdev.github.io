728e0fc1390f20c7f7621d953a06cf2476fe29ab
Dasayef <dscxd@outlook.com>
2016-03-04 20:22:30 -0500

    version1 mejorada

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,12 @@
 $(function() {
-
   changeSize();
-
   window.onresize = function(event) {
     changeSize();
   };
-
   function changeSize() {
-
     var screen = window.innerHeight;
     $(".slide").css("height", screen + 'px');
     $(".title").css("fontSize", screen / 15 + 'px');
     $(".description").css("fontSize", screen / 30 + 'px');
   }
-
 });
