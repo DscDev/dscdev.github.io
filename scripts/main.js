5ec482c451923ce69d618fe175faa4fb5fbb8478
Dasayef <dscxd@outlook.com>
2016-03-16 00:26:16 -0500

    version 3

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+$(function () {
+
+    $activo = $(".active");
+    $home = $("#home");
+    $about = $("#about");
+    $skills = $("#skills");
+
+    $(document).scroll(function () {
+        $pos = window.scrollY;
+        $alt = window.innerHeight;
+        if ($pos < 0.7 * $alt) {
+            $home.addClass("active");
+            $home.siblings().removeClass("active");
+        } else if ($pos >= 0.7 * $alt && $pos < 1.7 * $alt) {
+            $about.addClass("active");
+            $about.siblings().removeClass("active");
+
+        } else {
+            $skills.addClass("active");
+            $skills.siblings().removeClass("active");
+        }
+    });
+
+
+    $(".bars").click(function () {
+        $(".navegacion").toggle();
+        changeIcon();
+
+    })
+
+    function changeIcon() {
+        $icon = $(".bars").children();
+        if ($icon.hasClass("fa-bars")) {
+            $icon.removeClass("fa-bars");
+            $icon.addClass("fa-times");
+        } else {
+            $icon.removeClass("fa-times");
+            $icon.addClass("fa-bars");
+        }
+    }
+
+});
